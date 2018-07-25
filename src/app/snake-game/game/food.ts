3c3626046c13e8fe263b4fedec462ff375d659a6
Mark Heimer <mark.heimer@t2informatik.de>
2018-07-25 12:25:03 +0200

    responsive FoodDrawing

--- src/app/snake-game/game/food.ts
+++ src/app/snake-game/game/food.ts
@@ -20,11 +20,12 @@ export class Food implements CellObject {
     }
 
     draw(context: CanvasRenderingContext2D): void {
-        //context.fillRect(this.x * this.cellWidth, this.y * this.cellHeight, this.cellWidth, this.cellHeight);
         context.beginPath()
         context.arc(this.x* this.cellWidth + this.cellWidth/2,
                     this.y * this.cellHeight + this.cellHeight / 2,
-                    15,0,2* Math.PI,false);
+                    this.cellWidth/2,0,2* Math.PI,false);
+                    
+                    
         context.fillStyle = 'green';
         context.fill();
         context.stroke();
