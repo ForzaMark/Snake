962253dbaa1557cdd45987797d7d4984b0e774f8
Mark Heimer <mark.heimer@t2informatik.de>
2018-07-26 15:54:09 +0200

    first Draw

--- src/app/snake-game/game/snake.ts
+++ src/app/snake-game/game/snake.ts
@@ -8,6 +8,7 @@ export class Snake {
     private snakeParts: SnakePart[] = [];
     private snakeHead: SnakePart;
     private changeCounter = 0;
+    private headCenter: number[] = [0, 0];
 
     constructor(private fieldWidth: number,
                 private fieldHeight: number,
@@ -44,9 +45,37 @@ export class Snake {
             } else {
                 context.fillStyle = '#B18904';
             }
-            context.fillRect(this.snakeParts[i].x * cellWidth,
-                             this.snakeParts[i].y * cellHeight,
+            this.headCenter[0] =  this.snakeParts[i].x * cellWidth;
+            this.headCenter[1] =  this.snakeParts[i].y * cellHeight;
+            context.beginPath();
+            if ( i === 0 && this.snakeParts.length > 1) {
+                switch (this.direction) {
+                    case Direction.right:
+                        context.arc((this.headCenter[0]),
+                        (this.headCenter[1] + cellHeight / 2),
+                        (cellWidth / 2), 1.5 * Math.PI, (0.5 * Math.PI), false);
+                        break;
+                    case Direction.left:
+                        context.arc((this.headCenter[0] + cellWidth ),
+                        (this.headCenter[1] + cellHeight / 2),
+                        (cellWidth / 2), 0.5 * Math.PI, (1.5 * Math.PI), false);
+                        break;
+                    case Direction.down:
+                        context.arc((this.headCenter[0] + cellWidth / 2),
+                        (this.headCenter[1] ),
+                        (cellWidth / 2), 0 * Math.PI, (1 * Math.PI), false);
+                        break;
+                    case Direction.up:
+                        context.arc((this.headCenter[0] + cellWidth / 2),
+                        (this.headCenter[1] + cellHeight),
+                        (cellWidth / 2), 1 * Math.PI, (0 * Math.PI), false);
+                }
+            } else {
+                context.fillRect(this.headCenter[0],
+                             this.headCenter[1],
                              cellWidth, cellHeight);
+            }
+            context.fill();
          }
     }
     grow(): void {
